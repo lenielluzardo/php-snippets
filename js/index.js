2fb67db4286ab73af97eb4ec0b40b8db0334be4b
Leniel Luzardo <contact@lenielluzardo.com>
2021-01-21 14:37:47 -0300

    Backward Button

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
 let navMenu = document.querySelector('.nav_menu');
-let navIcon = document.querySelector('.nav_icon').addEventListener("click", e => {
+let navIcon = document.querySelector('.nav_icon');
 
-console.log(navMenu);
+console.log(navIcon);
+
+navIcon.addEventListener("click", e => {
 
   if(navMenu.classList.contains('hidden')){
     navMenu.classList.remove('hidden');
-}
+    navIcon.src='../assets/x_button.svg';
+  }
 else {
   navMenu.classList.add('hidden');
+  navIcon.src='./assets/hamb_button.svg';
 }
 });
