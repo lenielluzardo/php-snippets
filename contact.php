e1fb784a7c3b907295d4a7bdd948e5cfde54b5b8
Leniel Luzardo <contact@lenielluzardo.com>
2021-01-22 20:35:25 -0300

    Contact PHP Script Added

---
+++ contact.php
@@ -0,0 +1,62 @@
+<?php
+$to='lenielluzardo@gmail.com';
+$fullname='';
+$email='';
+$subject=array();
+$discipline=array();
+$comments='';
+$newsletter='';
+$isHuman='';
+
+if(isset($_POST['f_button'])){
+  $isValid = true;
+
+  if(!isset($_POST['fullname']) || $_POST['fullname'] === '') {
+    $isValid=false;
+  }
+  if(!isset($_POST['email']) || $_POST['email'] === ''){
+    $isValid=false;
+  }
+  if(!isset($_POST['subject']) || $_POST['subject'] === ''){
+    $isValid=false;
+  }
+  if(!isset($_POST['discipline']) || $_POST['discipline'] === ''){
+    $isValid=false;
+  }
+  if(!isset($_POST['comments']) || $_POST['comments'] === ''){
+    $isValid=false;
+  }
+  if(!isset($_POST['newsletter']) || $_POST['newsletter'] === ''){
+    $isValid=false;
+  }
+  if(!isset($_POST['is_human']) || $_POST['is_human'] === ''){
+    $isValid=false;
+  }
+  
+  if($isValid){
+
+    $fullname=htmlspecialchars($_POST['fullname'], ENT_QUOTES);
+    $email=htmlspecialchars($_POST['email'], ENT_QUOTES);
+    $subject=htmlspecialchars(implode(' ', $_POST['subject']), ENT_QUOTES);
+    $discipline=htmlspecialchars(implode(' ', $_POST['discipline']), ENT_QUOTES);
+    $comments=htmlspecialchars($_POST['comments'], ENT_QUOTES);
+    $newsletter=htmlspecialchars($_POST['newsletter'], ENT_QUOTES);
+    $isHuman=htmlspecialchars($_POST['is_human'], ENT_QUOTES);
+
+  echo($fullname.PHP_EOL);
+  echo($email.PHP_EOL);
+  echo($subject.PHP_EOL);
+  echo($discipline.PHP_EOL);
+  echo($comments.PHP_EOL);
+  echo($newsletter.PHP_EOL);
+  echo($isHuman.PHP_EOL);
+
+    // mail($to, $subject.' on '.$discipline, $comments, $emai,)
+  }
+  
+}
+
+
+
+
+?>
